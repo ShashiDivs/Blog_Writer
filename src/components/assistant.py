b72f25273f9b89d19ef96ae72eda35921b1cefa9
shashi <shashipolity@gmail.com>
2025-02-16 21:32:03 +0530

    comments rewritten

--- src/components/assistant.py
+++ src/components/assistant.py
@@ -1,6 +1,4 @@
 from langchain_core.messages import HumanMessage, SystemMessage
-
-
 from src.utils.blog_tools import title_creator, content_writer
 from src.constants.contants import llm
 
@@ -10,9 +8,8 @@ tools = [title_creator, content_writer]
 llm_with_tools = llm.bind_tools(tools, parallel_tool_calls=False)
 
 
-
 # System message for context
-sys_msg = SystemMessage(content="You are a helpful assistant for writing blogs.")
+sys_msg = SystemMessage(content="You are a helpful assistant for writing comprhensive blogs.")
 
 def assistant(state):
     # state should be a dictionary with "messages" as a list of messages
